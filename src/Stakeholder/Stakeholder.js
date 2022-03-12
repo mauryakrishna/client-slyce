fdf2f6e3fc5e5e0149cb6f775472f9db17cc9f2e
Krishna maurya <kmaurya@intraedge.com>
2022-03-13 01:13:52 +0530

    stakeholder display styling

--- src/Stakeholder/Stakeholder.js
+++ src/Stakeholder/Stakeholder.js
@@ -5,12 +5,12 @@ const Stakeholder = ({ stakeholderData, documents, buyerOrSeller }) => {
 
   return (
     <div className="container">
-      <div><span>Name: </span><span>{name}</span></div>
-      <div><span>Email: </span><span>{email}</span></div>
-      <div><span colSpan={2} className="stakeholder-involvement">{buyerOrSeller}</span></div>
+      <div className="row"><span>Name: </span><span>{name}</span></div>
+      <div className="row"><span>Email: </span><span>{email}</span></div>
+      <div className="row"><span colSpan={2} className="stakeholder-involvement">{buyerOrSeller}</span></div>
       {
         documents.map((doc) => {
-          return <div key={doc.id}>
+          return <div key={doc.id} className="row">
             <span>{doc.type.split('_').join(" ")}:</span> <span>{doc[`${buyerOrSeller}Status`]}</span></div>
         })
       }
